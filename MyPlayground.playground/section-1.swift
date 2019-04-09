31eb2f49ec844f452e8b3175d276111ab1760dd2
Martin Tracey <matracey@outlook.ie>
2019-04-09 18:41:45 +0100

    Initial commit

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,17 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+str = "Hello, funtime peoples"
+
+let programmingLanguage = "Swift"
+
+programmingLanguage.uppercaseString
+
+var greeting = "Hello " + programmingLanguage + " you piece of shit. "
+
+str + programmingLanguage;
+
+
+
